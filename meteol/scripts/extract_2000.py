dbd3ca4715c7711e7682f60cc53c4124a56ee499
Ytalo-Douglas-ss <121064190+Syds674@users.noreply.github.com>
2023-12-27 16:43:03 -0300

    Update extract_2000.py

--- meteol/scripts/extract_2000.py
+++ meteol/scripts/extract_2000.py
@@ -21,7 +21,7 @@ def get_csv(file_path):
     return df
 
 # Caminho local para salvar o arquivo ZIP
-local_zip_path = "/hdfs/data/order/tmp/dadoszip"
+local_zip_path = "/hdfs/data/order/tmp/dados_brutos"
 
 # Caminho local para extrair os arquivos CSV
 local_extract_path = "/hdfs/data/order/tmp/dados_temp"
