0d14e3dbce4e1a79c95f46c2208c79748754e332
Ytalo-Douglas-ss <121064190+Syds674@users.noreply.github.com>
2023-12-28 14:52:23 -0300

    Update extract_2000.py

--- meteol/scripts/extract_2000.py
+++ meteol/scripts/extract_2000.py
@@ -6,6 +6,13 @@ import os
 def download_and_extract_csv(url, local_zip_path, local_extract_path):
     # Baixar o arquivo ZIP
     response = requests.get(url)
+
+    # Certificar-se de que o diretório pai existe
+    local_zip_dir = os.path.dirname(local_zip_path)
+    if not os.path.exists(local_zip_dir):
+        print("Diretório dados_brutos não existe")
+        os.makedirs(local_zip_dir)
+
     with open(local_zip_path, "wb") as zip_file:
         zip_file.write(response.content)
 
@@ -21,17 +28,15 @@ def get_csv(file_path):
     return df
 
 # Caminho local para salvar o arquivo ZIP
-local_zip_path = "/hdfs/data/order/tmp/dados_brutos"
+local_zip_path = "/hdfs/data/order/tmp/dados_brutos.zip"
 
 # Caminho local para extrair os arquivos CSV
 local_extract_path = "/hdfs/data/order/tmp/dados_temp"
 
 # Caminho no HDFS para armazenar os arquivos CSV
-hdfs_path = "/hdfs/data/order/2000"  # Substitua 'seu_usuario' pelo seu nome de usuário no HDFS
-
+hdfs_path = "/hdfs/data/order/2000"
 
 date_link = '2000'  # Nome do arquivo ZIP
-
 
 try:
     # Baixar e extrair o CSV diretamente para a pasta local no HDFS
@@ -55,8 +60,11 @@ except Exception as e:
     print("ERRO:", e)
 
 finally:
+    # Verificar a existência do diretório antes de removê-lo
+    if os.path.exists(local_zip_path):
+        os.remove(local_zip_path)
+
     # Remover os arquivos temporários
-    os.remove(local_zip_path)
     for root, dirs, files in os.walk(local_extract_path):
         for file in files:
             os.remove(os.path.join(root, file))
