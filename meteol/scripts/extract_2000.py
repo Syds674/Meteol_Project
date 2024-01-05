b0ae3d1531da44b8254255048bb6e13de3a04d1c
unknown <ytalodouglass@hotmail.com>
2024-01-05 15:38:10 -0300

    modificação no no arquivo de extração de dados

--- meteol/scripts/extract_2000.py
+++ meteol/scripts/extract_2000.py
@@ -1,47 +1,78 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
 from pyspark.sql import functions as F
 import requests
 from zipfile import ZipFile
-import os
 import shutil
 
-# Criar SparkSession
-spark = SparkSession.builder.appName('Get - CSV').getOrCreate()
+def download_data(url, local_zip_path):
+    # Criar SparkSession
+    spark = SparkSession.builder.appName('Download - CSV').getOrCreate()
 
-def download_and_extract_csv(url, local_zip_path, local_extract_path):
-    # Baixar o arquivo ZIP
-    response = requests.get(url)
+    try:
+        # Baixar o arquivo ZIP
+        response = requests.get(url)
 
-    # Certificar-se de que o diretório pai existe
-    local_zip_dir = spark._jvm.java.io.File(local_zip_path).getParentFile()
-    local_zip_dir.mkdirs()
+        # Certificar-se de que o diretório pai existe
+        local_zip_dir = spark._jvm.java.io.File(local_zip_path).getParentFile()
+        local_zip_dir.mkdirs()
 
-    with open(local_zip_path, "wb") as zip_file:
-        zip_file.write(response.content)
+        with open(local_zip_path, "wb") as zip_file:
+            zip_file.write(response.content)
 
-    # Extrair o conteúdo do ZIP diretamente para o local_extract_path
-    with ZipFile(local_zip_path, "r") as zip_ref:
-        zip_ref.extractall(local_extract_path)
+    except Exception as e:
+        print("ERRO no download:", e)
+
+    finally:
+        spark.stop()  # Parar a SparkSession após o download
+
+def extract_csv(local_zip_path, local_extract_path):
+    # Criar SparkSession
+    spark = SparkSession.builder.appName('Extract - CSV').getOrCreate()
+
+    try:
+        # Extrair o conteúdo do ZIP diretamente para o local_extract_path
+        with ZipFile(local_zip_path, "r") as zip_ref:
+            for file_info in zip_ref.infolist():
+                file_info.filename = file_info.filename.encode('cp437').decode('utf-8')  # Corrigir codificação para Spark
+                zip_ref.extract(file_info, local_extract_path)
+
+    except Exception as e:
+        print("ERRO na extração:", e)
+
+    finally:
+        spark.stop()  # Parar a SparkSession após a extração
 
 def get_csv(file_path):
-    df = spark.read.format("csv").option("header", True).load(file_path)
-    return df
+    # Criar SparkSession
+    spark = SparkSession.builder.appName('Read - CSV').getOrCreate()
+
+    try:
+        df = spark.read.format("csv").option("header", True).load(file_path)
+        return df
+
+    except Exception as e:
+        print("ERRO na leitura do CSV:", e)
+
+    finally:
+        spark.stop()  # Parar a SparkSession após a leitura do CSV
 
 # Caminho local para salvar o arquivo ZIP
-local_zip_path = "/hdfs/data/order/tmp/dados_brutos"
+local_zip_path = "hdfs:///hdfs/data/order/tmp/dados_brutos"
 
 # Caminho local para extrair os arquivos CSV
-local_extract_path = "/hdfs/data/order/tmp/dados_temp"
+local_extract_path = "hdfs:///hdfs/data/order/tmp/dados_temp"
 
 # Caminho no HDFS para armazenar os arquivos CSV
-hdfs_path = "/hdfs/data/order/2000"
+hdfs_path = "hdfs:///hdfs/data/order/2000"
 
 date_link = '2000'  # Nome do arquivo ZIP
 
 try:
-    # Baixar e extrair o CSV diretamente para a pasta local no HDFS
-    download_and_extract_csv("https://portal.inmet.gov.br/uploads/dadoshistoricos/{}.zip".format(date_link), local_zip_path, local_extract_path)
+    # Baixar o CSV diretamente para a pasta local no HDFS
+    download_data("https://portal.inmet.gov.br/uploads/dadoshistoricos/{}.zip".format(date_link), local_zip_path)
+
+    # Extrair o CSV
+    extract_csv(local_zip_path, local_extract_path)
 
     # Listar arquivos extraídos
     csv_files = [f for f in spark._jvm.java.io.File(local_extract_path).list() if f.endswith('.CSV')]
@@ -65,12 +96,12 @@ except Exception as e:
 finally:
     try:
         # Remover os arquivos temporários
-        if os.path.exists(local_extract_path):
-            shutil.rmtree(local_extract_path)
+        if spark._jvm.java.nio.file.Files.exists(spark._jvm.java.nio.file.Paths.get(local_extract_path)):
+            spark._jvm.java.nio.file.Files.delete(spark._jvm.java.nio.file.Paths.get(local_extract_path))
 
         # Verificar a existência do arquivo antes de removê-lo
-        if os.path.exists(local_zip_path):
-            os.remove(local_zip_path)
+        if spark._jvm.java.nio.file.Files.exists(spark._jvm.java.nio.file.Paths.get(local_zip_path)):
+            spark._jvm.java.nio.file.Files.delete(spark._jvm.java.nio.file.Paths.get(local_zip_path))
         
         print("ARQUIVOS TEMPORÁRIOS EXCLUÍDOS")
     except Exception as e:
